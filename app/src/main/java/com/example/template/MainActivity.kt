0f308c7110e67c6efe2f636f3a4b75f398bda2e1
Ibrahimhussien227 <65757724+Ibrahimhussien227@users.noreply.github.com>
2020-10-12 00:19:23 +0300

    Fragment

--- app/src/main/java/com/example/template/MainActivity.kt
+++ app/src/main/java/com/example/template/MainActivity.kt
@@ -1,38 +1,77 @@
 package com.example.template
 
-import android.content.Context
-import android.content.Intent
-import android.content.SharedPreferences
+
 import android.os.Bundle
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_sign_up.*
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var homeFragment: HomeFragment
+    lateinit var favoriteFragment: FavoriteFragment
+    lateinit var profileFragment: ProfileFragment
+    lateinit var searchFragment: SearchFragment
+    lateinit var addPhotoFragment: AddPhotoFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_Send.setOnClickListener {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.btn_bottomNavigation)
+        bottomNavigationView.setOnNavigationItemReselectedListener { item ->
 
-            val intent = Intent(Intent.ACTION_SEND)
-            intent.setType("text/plain")
-            val editText = findViewById<EditText>(R.id.et_message)
-            val message = et_message.text.toString()
+            homeFragment = HomeFragment()
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.frame_layout, homeFragment)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .commit()
 
-            intent.putExtra(Intent.EXTRA_TEXT,message)
-
-            if (intent.resolveActivity(packageManager) != null)
-
-                startActivity(intent)
-
-        }
-        btn_secondAcitvity.setOnClickListener {
-            val intent = Intent(this, Profile::class.java)
-            startActivity(intent)
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    homeFragment = HomeFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, homeFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.navigation_profile -> {
+                    profileFragment = ProfileFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, profileFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.navigation_search -> {
+                    searchFragment = SearchFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, searchFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.navigation_favorite -> {
+                    favoriteFragment = FavoriteFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, favoriteFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.navigation_addPhoto -> {
+                    addPhotoFragment = AddPhotoFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, addPhotoFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+            }
         }
     }
 }
